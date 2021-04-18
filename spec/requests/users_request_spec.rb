88e5cbdadd47a8d56e249b3c2e4d1fb528a36c56
Dennis Tanaka <4251238+dennistanaka@users.noreply.github.com>
2021-04-18 08:31:11 +0000

    Add test to check unsuccessful signups

--- spec/requests/users_request_spec.rb
+++ spec/requests/users_request_spec.rb
@@ -9,4 +9,15 @@ RSpec.describe "Users", type: :request do
     end
   end
 
+  describe "POST invalid signup information" do
+    let(:create_user) { post "/users", params: { user: { name: "",
+                                                         email: "user@invalid",
+                                                         password: "foo",
+                                                         password_confirmation: "bar" } } }
+
+    it "should not create change user count" do
+      expect { create_user }.to_not change { User.count }
+    end
+  end
+
 end
